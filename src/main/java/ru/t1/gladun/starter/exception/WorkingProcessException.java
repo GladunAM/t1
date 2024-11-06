3326685d19d0c1af3771f4c394d4c7bf375781ee
GladunAM <gladun.anastasiia.91@gmail.com>
2024-11-06 18:47:49 +0300

    Revert "LogApp"
    This reverts commit b5fb34246225a27a33b8845e4148a8725321ae87.

--- src/main/java/ru/t1/gladun/starter/exception/WorkingProcessException.java
+++
@@ -1,7 +0,0 @@
-package ru.t1.gladun.starter.exception;
-
-public class WorkingProcessException extends RuntimeException {
-    public WorkingProcessException(String message) {
-        super(message);
-    }
-}
