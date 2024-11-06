e13d36fbbdcc2c0b2dc217e58fb1e1aaed38a3a3
GladunAM <gladun.anastasiia.91@gmail.com>
2024-11-06 18:46:52 +0300

    LogApp

---
+++ src/main/java/ru/t1/gladun/starter/exception/WorkingProcessException.java
@@ -0,0 +1,7 @@
+package ru.t1.gladun.starter.exception;
+
+public class WorkingProcessException extends RuntimeException {
+    public WorkingProcessException(String message) {
+        super(message);
+    }
+}
